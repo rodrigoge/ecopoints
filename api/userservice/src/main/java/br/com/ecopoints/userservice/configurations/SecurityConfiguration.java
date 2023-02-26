6a762253c069aba8415c9080ab4b02f4a9d22d26
Rodrigo Gouveia <rgestevao@gmail.com>
2023-02-25 22:56:07 -0300

    Feature: encrypt password and security configuration

--- api/userservice/src/main/java/br/com/ecopoints/userservice/configurations/SecurityConfiguration.java
+++ api/userservice/src/main/java/br/com/ecopoints/userservice/configurations/SecurityConfiguration.java
@@ -4,21 +4,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfiguration {
+public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests((auth) -> auth
-                        .anyRequest()
-                        .authenticated()
-                )
+        httpSecurity.authorizeHttpRequests(
+                        (authz) -> authz.anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().anyRequest();
     }
 }
