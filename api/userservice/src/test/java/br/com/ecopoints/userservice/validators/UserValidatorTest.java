8eb31828723190f71bf0e9b0a8b85d339c40deab
Rodrigo Gouveia <rgestevao@gmail.com>
2023-02-27 20:23:12 -0300

    Testing: keeping 80% for unitary tests

--- api/userservice/src/test/java/br/com/ecopoints/userservice/validators/UserValidatorTest.java
+++
@@ -1,9 +0,0 @@
-package br.com.ecopoints.userservice.validators;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-@ExtendWith(MockitoExtension.class)
-public class UserValidatorTest {
-
-}
